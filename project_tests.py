bf90e69f226cca8b26c4535af180672cdfcf6294
serajbel <163426040+serajbel@users.noreply.github.com>
2024-04-22 18:29:40 -0400

    Update project_tests.py

--- project_tests.py
+++ project_tests.py
@@ -4,7 +4,7 @@ import project1
 #Testing the ticket class
 
 def test_ticket():
-  ticket1 = Ticket(5, 100)
+  ticket1 = Ticket(5, 'easy')
   assert project1.check_ticket(5) == 'You win'
 
 #Testing the person class

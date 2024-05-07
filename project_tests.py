09f13ad973bc1e52b518ef0f1c9b900afdc4b77f
loakes1 <157723140+loakes1@users.noreply.github.com>
2024-05-07 13:30:19 -0400

    Add files via upload

--- project_tests.py
+++ project_tests.py
@@ -4,21 +4,26 @@ import project1
 #Testing the ticket class
 
 def test_ticket():
-  ticket1 = Ticket(503, 'easy')
-  assert project1.check_ticket(ticket1, 503) == 100
-  assert project1.check_ticket(ticket1, 499) == 50
-  assert project1.check_ticket(ticket1, 495) == 10
-  assert project1.check_ticket(ticket1, 475) == 0
-  ticket2 = Ticket(500, 'hard')
-  assert project1.check_ticket(ticket1, 500) == 1000
-  assert project1.check_ticket(ticket1, 490) == 500
-  assert project1.check_ticket(ticket1, 455) == 100
-  assert project1.check_ticket(ticket1, 340) == 0
+  ticket1 = project1.Ticket(503, 'easy')
+  assert ticket1.check_ticket(503) == 100
+  assert ticket1.check_ticket(499) == 50
+  assert ticket1.check_ticket(495) == 10
+  assert ticket1.check_ticket(475) == 0
+  ticket2 = project1.Ticket(500, 'hard')
+  assert ticket2.check_ticket(500) == 1000
+  assert ticket2.check_ticket(490) == 500
+  assert ticket2.check_ticket(455) == 100
+  assert ticket2.check_ticket(340) == 0
 
 #Testing the person class
 
 def test_person():
-  person1 = Person('test_person') 
+  person1 = project1.Player('test_person') 
   assert person1.get_number_tickets(20, 4) == 5
-  person1.add_ticket(4, 'easy')
-  assert len(person1.tickets) == 4
+  #person1.add_ticket('easy')
+  #assert len(person1.tickets) == 1
+
+#Testing the main function
+
+def test_main():
+  pass
